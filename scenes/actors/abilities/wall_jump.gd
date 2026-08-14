e46c39b0f260cac8633f5d5d39a239357347bda4
Josh White <ciph3rzer0@gmail.com>
2026-08-13 23:34:51 -0400

    fix: wall jump direction

--- scenes/actors/abilities/wall_jump.gd
+++ scenes/actors/abilities/wall_jump.gd
@@ -7,17 +7,17 @@ func process_input(_state: PlayerActor.State, _delta: float) -> void:
 	super.process_input(_state, _delta)
 	
 	if _state.is_on_wall and _state.frame_input.consume_jump_press():
+		var wall_normal := actor.get_wall_normal()
+		wall_normal.y = 0
+		wall_normal = wall_normal.normalized()
 		
-		var normal = actor.get_wall_normal() #* Vector3(1, 0, 1)
-		#normal = normal.normalized()
-		normal = normal.rotated(Vector3.RIGHT, 1/4 * PI)
+		var jump_dir := (wall_normal + Vector3.UP).normalized()
 		
 		_state.time_on_floor = 0
 		_state.last_jump = 0.0
 		_state.consecutive_jumps = 1
-		actor.velocity = actor.JUMP_VELOCITY * normal
+		actor.velocity = actor.JUMP_VELOCITY * jump_dir
 		
 		await get_tree().process_frame
 		actor.anim.speed_scale = 1
-		
 		actor.anim.play(&"wall-kick")
