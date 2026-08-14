f0248528c4af7302a3abd7ac15ded2b5846035f3
Josh White <ciph3rzer0@gmail.com>
2026-08-14 14:34:34 -0400

    fix: bugs and organize player_actor.gd

--- scenes/actors/mario/player_actor.gd
+++ scenes/actors/mario/player_actor.gd
@@ -1,7 +1,10 @@
 class_name PlayerActor
 extends CharacterBody3D
-
 signal on_player_state_frame(frame_state: PlayerActor.State)
+
+const VEC3_XZ := Vector3(1, 0, 1)
+const PROGRESS_CHARS := ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
+const SKIDDING_ANGLE := cos(deg_to_rad(160))
 
 @export var MAX_MOVE_SPEED := 8.0
 @export var ACCELERATION := 12.0
@@ -10,12 +13,12 @@ signal on_player_state_frame(frame_state: PlayerActor.State)
 @export var GRAVITY_ACCEL := 2.0
 @export var JUMP_CHAIN_WINDOW := 0.4
 
-const PROGRESS_CHARS = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
-const SKIDDING_ANGLE = cos(deg_to_rad(160))
-
 @onready var anim: AnimationPlayer = get_node("mario/AnimationPlayer") as AnimationPlayer
 
 var current_state: PlayerActor.State = State.new()
+
+var current_speed: float
+var target_speed: float
 
 func _ready() -> void:
 	assert(anim != null)
@@ -35,25 +38,45 @@ func pre_process_input(frame_input: PlayerFrameInput, delta: float) -> void:
 	# Is the player not holding any direction?
 	var zero_move_input = frame_input.move_vector.is_equal_approx(Vector2.ZERO)
 	
-	# Calculate acceleration
-	var move_accel : Vector3
-	if zero_move_input:
-		# Drag Force
-		move_accel = -velocity * (Vector3.ONE - Vector3.UP) * DRAG_ACCEL
-	if current_state.is_skidding:
-		# Drag Force
-		move_accel = -velocity * (Vector3.ONE - Vector3.UP) * DRAG_ACCEL * 2
-	else:
-		# Move Force
-		move_accel = move_vector * ACCELERATION
+	#region Acceleration
+	# Target Speed is determined by distance analog stick is pressed
+	target_speed = frame_input.move_vector.length() * MAX_MOVE_SPEED
+	# Get current horizontal speed
+	current_speed = (velocity * VEC3_XZ).length()
 	
+	var speed_diff := target_speed - current_speed
+	var speed_change := clampf(absf(speed_diff), 0.0, ACCELERATION * delta) * signf(speed_diff)
+	current_speed += speed_change
+	
+	#endregion
+	print("TS        ", target_speed)
+	print("CS        ", current_speed)
+	print("Speed Ch  ", speed_change)
+	
+	## Calculate acceleration
+	var move_accel : Vector3
+	#if zero_move_input:
+		## Drag Force
+		#move_accel = -velocity * VEC3_XZ * DRAG_ACCEL
+	#if current_state.is_skidding:
+		## Drag Force
+		#move_accel = -velocity * VEC3_XZ * DRAG_ACCEL * 2
+	#else:
+		## Move Force
+		#move_accel = move_vector * ACCELERATION
+	
+	print("move_vector * current_speed  ", move_vector, " * ", current_speed, " = ", move_vector * current_speed)
+	move_accel = move_vector * current_speed
 	# Apply acceleration to velocity
-	velocity += (move_accel) * delta
+	var horizontal := velocity * VEC3_XZ
+	if not horizontal.is_zero_approx():
+		var direction := horizontal.normalized()
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
 	
 	var PREFIX = PROGRESS_CHARS[(Time.get_ticks_msec() / 100) % PROGRESS_CHARS.size()]
-	#print("%s SPEED: %5.2f / %d.  Move: %5.2f" % [PREFIX, velocity.length(), MAX_MOVE_SPEED, move_accel.length()])
+	print("%s SPEED: %5.2f / %d.  Move: %5.2f" % [PREFIX, velocity.length(), MAX_MOVE_SPEED, move_accel.length()])
 	
-	var horizontal := Vector3(velocity.x, 0, velocity.z)
 	if horizontal.length() > MAX_MOVE_SPEED:
 		horizontal = horizontal.normalized() * MAX_MOVE_SPEED
 		velocity.x = horizontal.x
@@ -62,14 +85,16 @@ func pre_process_input(frame_input: PlayerFrameInput, delta: float) -> void:
 		velocity.x = 0
 		velocity.z = 0
 	
-	# Look at direction
+	#region Look At
 	var look_at_target = velocity
 	look_at_target.y = 0
 	look_at_target += global_position
 	
 	if !global_position.is_equal_approx(look_at_target):
 		look_at(look_at_target, Vector3.UP)
+	#endregion
 	
+	#region Gravity
 	var gravity_to_apply := 0.0
 	
 	if velocity.y < 0:
@@ -82,7 +107,12 @@ func pre_process_input(frame_input: PlayerFrameInput, delta: float) -> void:
 		gravity_to_apply -= GRAVITY_ACCEL * 0.5
 	
 	velocity.y -= gravity_to_apply
+	#endregion
 	
+	if current_state.is_skidding:
+		if horizontal.is_zero_approx():
+			anim.speed_scale = 1
+			anim.play(&"quick-turn")
 	if is_on_floor():
 		if horizontal.is_zero_approx():
 			anim.speed_scale = 1
