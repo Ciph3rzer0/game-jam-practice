74944dc5a2da04dc0a8827a841e0efe45bd8ed6f
Josh White <ciph3rzer0@gmail.com>
2026-08-14 15:24:35 -0400

    feat: some camera controls (not working)

--- scenes/camera/lakitu_camera.gd
+++ scenes/camera/lakitu_camera.gd
@@ -1,3 +1,4 @@
+class_name Lakitu
 extends Node3D
 
 @export var camera_target: PlayerActor
@@ -5,10 +6,21 @@ extends Node3D
 @export var CAMERA_HEIGHT := 4.0
 @export var CAMERA_SPEED := 2.0
 @export var ORBIT_SPEED := 1.0
+@export var AUTO_CAM_RESET_TIME := 0
+
+var target_camera_angle: float
+var auto_cam_timer: float
+var cam_rotation: float
+
+func player_rotate_camera(angle):
+	auto_cam_timer = AUTO_CAM_RESET_TIME
+	cam_rotation = global_rotation.y
+	cam_rotation += angle
 
 func _physics_process(delta: float) -> void:
+	auto_cam_timer -= delta
+	
 	if camera_target:
-		
 		var is_actor_moving_up: bool = camera_target.current_state.vertical_speed > 0
 		var camera_target_offset_scale: float = 0 if is_actor_moving_up else 1
 		var camera_target_offset = Vector3(0, CAMERA_HEIGHT, 0) * camera_target_offset_scale
@@ -20,4 +32,7 @@ func _physics_process(delta: float) -> void:
 		
 		# LERP Camera *ROTATION* towards target rotation
 		var target_basis := camera_target.global_transform.basis.orthonormalized()
+		if auto_cam_timer > 0:
+			target_basis = Basis(Vector3.UP, cam_rotation)
+		
 		global_transform.basis = global_transform.basis.orthonormalized().slerp(target_basis, ORBIT_SPEED * delta)
